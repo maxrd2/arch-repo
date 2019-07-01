b4728014ede69b5577e772873f31e9337979b9e1
Mladen Milinkovic <maxrd2@smoothware.net>
2019-07-01 17:09:58 +0200

    Added pacaur, dropbox, megasync PKGBUILDs

---
+++ packages/common/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+. "$HOME/bin/include-common.sh"
+
+sect "Adding GPG keys"
+gpg-add FC918B335044912E 
+
+sect "Updating pacman databases"
+[[ ! -L "$repodir/maxrd2.db" ]] && ln -fs "maxrd2.db.tar.gz" "$repodir/maxrd2.db"
+sudo pacman -Syu --noconfirm --needed
+
+pkg-build auracle-git
+pkg-build pacaur
+pkg-build dropbox
+pkg-build megasync
